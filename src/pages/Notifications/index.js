9538960147c7445ec3e0cb3c65059ab680fcc183
unknown <antonio.gally@gmail.com>
2021-03-17 23:06:15 -0300

    Adding Notifications page

--- src/pages/Notifications/index.js
+++ src/pages/Notifications/index.js
@@ -1,10 +1,60 @@
 import React from "react";
-import { View } from "react-native";
-
-// import { Container } from './styles';
+import Like from "../../assets/Svgs/Notifications/Like.svg";
+import Chat from "../../assets/Svgs/Notifications/Chat.svg";
+import {
+  Container,
+  Header,
+  Title,
+  NotificationsContent,
+  Card,
+  Text,
+} from "./styles";
 
 const Notifications = () => {
-  return <View />;
+  const list = [
+    { action: "like", text: "Antônio" },
+    { action: "like", text: "David" },
+    { action: "comment", text: "Antônio" },
+    { action: "like", text: "Antônio" },
+    { action: "comment", text: "Shake" },
+    { action: "like", text: "Antônio" },
+    { action: "like", text: "David" },
+    { action: "comment", text: "Antônio" },
+    { action: "like", text: "Antônio" },
+    { action: "comment", text: "Shake" },
+    { action: "like", text: "Antônio" },
+    { action: "like", text: "David" },
+    { action: "comment", text: "Antônio" },
+    { action: "like", text: "Antônio" },
+    { action: "comment", text: "Shake" },
+  ];
+  return (
+    <>
+      <Container>
+        <Header>
+          <Title>Notificações</Title>
+        </Header>
+        <NotificationsContent>
+          {list.map((information, index) => (
+            <Card key={index}>
+              {information.action === "like" && (
+                <>
+                  <Like />
+                  <Text>{information.text} curtiu seu post</Text>
+                </>
+              )}
+              {information.action === "comment" && (
+                <>
+                  <Chat />
+                  <Text>{information.text} comentou seu post</Text>
+                </>
+              )}
+            </Card>
+          ))}
+        </NotificationsContent>
+      </Container>
+    </>
+  );
 };
 
 export default Notifications;
---
+++ src/pages/Notifications/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+`;
+export const Header = styled.View`
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 16px;
+  border-bottom-color: #eeeeee;
+  border-bottom-width: 1px;
+`;
+export const Title = styled.Text`
+  font-family: "WorkSans_500Medium";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 27px;
+  color: rgba(0, 0, 0, 0.85);
+`;
+export const NotificationsContent = styled.ScrollView`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+export const Card = styled.TouchableOpacity`
+  border-bottom-color: #eeeeee;
+  border-bottom-width: 1px;
+  padding-left: 32px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const Text = styled.Text`
+  font-family: "WorkSans_400Regular";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  color: rgba(0, 0, 0, 0.85);
+  margin-left: 16px;
+`;
